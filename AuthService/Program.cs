dfdd3cc4afbe924921d888eda15d853cbfbbd7f7
Will Lo <darthhun54@gmail.com>
2024-07-15 22:59:11 +0800

    change the auth docker file in dev mode and push the container

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -44,12 +44,12 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+    //app.UseDeveloperExceptionPage();
+//}
 
 app.UseHttpsRedirection();
 
--- AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79617d17881cd12dc768147fbd59d65fae9e6306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9b30edd0f3faf72d1cf657e9e58c251f98097c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
